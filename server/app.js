d072936d095a9b09668b900dcff8f4dd7fe70721
Stanley Huang <infamous_godhand@yahoo.com>
2020-05-27 19:38:51 +1000

    not working as expected

--- server/app.js
+++ server/app.js
@@ -1,9 +1,30 @@
 import serverless from "serverless-http";
 import express from "express";
-const app = express();
+import next from "next";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
+const startServer = async () => {
+  try {
+    await app.prepare();
+    const server = express();
 
-exports.handler = serverless(app);
+    //server.use(cookieParser());
+
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(8000, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:8000`);
+    });
+  } catch (err) {
+    console.log(err.stack);
+    process.exit(1);
+  }
+};
+
+//startServer();
+
+exports.handler = serverless(startServer);
